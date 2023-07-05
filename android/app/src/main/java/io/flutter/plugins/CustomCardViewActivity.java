6a2d7791ce294cbd819b5b8e7ebe6fe402791f28
slingneobank <project12sling@gmail.com>
2023-07-05 16:52:57 +0530

    customcard

--- android/app/src/main/java/io/flutter/plugins/CustomCardViewActivity.java
+++ android/app/src/main/java/io/flutter/plugins/CustomCardViewActivity.java
@@ -1,9 +1,8 @@
-public import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Switch;
+import android.widget.SwitchCompat;
 
 import com.pinelabs.pineperks.interfaces.CardManagerObserver;
 import com.pinelabs.pineperks.screens.components.CardCvvTextView;
@@ -14,14 +13,22 @@ import com.pinelabs.pineperks.sdk.CardManager;
 import com.pinelabs.pineperks.sdk.PineLabsSDK;
 import com.pinelabs.pineperks.sdk.models.PLCardResponse;
 
-import android.widget.SwitchCompat;
 import android.app.ProgressDialog;
+import android.widget.Toast;
 
-public class CustomCardViewActivity extends AppCompatActivity implements CardManagerObserver {
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.engine.FlutterEngineCache;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+
+public class CustomCardViewActivity extends AppCompatActivity implements CardManagerObserver, MethodChannel.MethodCallHandler {
     private CardManager cardManager;
     private static final String workingKey = "<GIVEN BY PINE LABS TEAM>";
     private static final String USERNAME = "<GIVEN BY PINE LABS TEAM>";
     private static final String URI = "<GIVEN BY PINE LABS TEAM>";
+
+    private ProgressDialog progressDialog;
+    private static final String CHANNEL = "my_channel";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,14 +41,23 @@ public class CustomCardViewActivity extends AppCompatActivity implements CardMan
         initialize();
 
         // Handle the toggle switch for showing/hiding card details
-        Switch showCardSwitch = findViewById(R.id.showCardToggle);
+        SwitchCompat showCardSwitch = findViewById(R.id.showCardToggle);
         showCardSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
             if (isChecked) {
-                showCardDetails();
+                progressDialog = new ProgressDialog(this);
+                progressDialog.setMessage("Loading...");
+                progressDialog.show();
+                // Ask SDK to render card details in custom view components
+                cardManager.showCardDetails(this);
             } else {
-                maskCardDetails();
+                cardManager.maskCardDetails(this);
             }
         });
+
+        // Initialize the MethodChannel
+        FlutterEngine flutterEngine = FlutterEngineCache.getInstance().get("flutter_engine");
+        MethodChannel methodChannel = new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL);
+        methodChannel.setMethodCallHandler(this);
     }
 
     // Initialize the encrypted clientKey, cardReferenceNumber, and username
@@ -52,26 +68,6 @@ public class CustomCardViewActivity extends AppCompatActivity implements CardMan
         String encryptedReferenceNumber = encrypt("<CARD_REFERENCE_NUMBER>", workingKey);
         cardManager.setCredentials(encryptedClientKey, encryptedReferenceNumber, USERNAME);
     }
-    
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_custom_card_view);
-
-    SwitchCompat showCardSwitch = findViewById(R.id.showCardToggle);
-    showCardSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
-        if (isChecked) {
-            progressDialog = new ProgressDialog(this);
-            progressDialog.setMessage("loading...");
-            progressDialog.show();
-            // ask SDK to render card details in custom view components
-            cardManager.showCardDetails(this);
-        } else {
-            cardManager.maskCardDetails(this);
-        }
-    });
-}
-
 
     // Show the card details in the custom views
     private void showCardDetails() {
@@ -87,6 +83,7 @@ public class CustomCardViewActivity extends AppCompatActivity implements CardMan
     public void onReceive(PLCardResponse plCardResponse) {
         int responseCode = plCardResponse.getResponseCode();
         String responseMessage = plCardResponse.getResponseMessage();
+
         // Handle the card response based on the response code and message
         // ...
 
@@ -104,13 +101,23 @@ public class CustomCardViewActivity extends AppCompatActivity implements CardMan
         }
     }
 
-    // Helper method to encrypt the data using working key
+    // Helper method to encrypt the data using the working key
     private String encrypt(String data, String workingKey) {
         // Implement the encryption logic using the workingKey
         // ...
-        return encryptedData;
+        return "encryptedData";
     }
-}
- {
-    
+
+    @Override
+    public void onMethodCall(MethodCall call, MethodChannel.Result result) {
+        if (call.method.equals("showCardDetails")) {
+            showCardDetails();
+            result.success(null);
+        } else if (call.method.equals("maskCardDetails")) {
+            maskCardDetails();
+            result.success(null);
+        } else {
+            result.notImplemented();
+        }
+    }
 }
--- lib/home/CustomCardView.dart
+++ lib/home/CustomCardView.dart
@@ -1,29 +1,98 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
-class CustomCardView extends StatelessWidget {
+
+final MethodChannel _methodChannel = MethodChannel('my_channel');
+
+class CustomCardView extends StatefulWidget {
+  const CustomCardView({super.key});
+
   @override
-  Widget build(BuildContext context) {
-    return AndroidView(
-      viewType: 'custom_card_view',
-      creationParams: {},
-      creationParamsCodec: const StandardMessageCodec(),
-    );
-  }
+  State<CustomCardView> createState() => _CustomCardViewState();
 }
-class showcard extends StatelessWidget {
-  const showcard({super.key});
+
+class _CustomCardViewState extends State<CustomCardView> {
+  bool _showCardDetails = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Card Details'),
+      ),
       body: Center(
-          child: CustomCardView(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Card Details',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _toggleCardDetails,
+              child: Text(_showCardDetails ? 'Hide Details' : 'Show Details'),
+            ),
+            SizedBox(height: 20),
+            Visibility(
+              visible: _showCardDetails,
+              child: FutureBuilder<Map<String, dynamic>?>(
+                  future: _getCardDetails(),
+                  builder: (BuildContext context, AsyncSnapshot<Map<String, dynamic>?> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return CircularProgressIndicator();
+                    } else if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}');
+                    } else {
+                      final cardDetails = snapshot.data;
+                      return Column(
+                        children: [
+                          Text('Card Number: ${cardDetails?['cardNumber'] ?? ''}'),
+                          Text('Issue Date: ${cardDetails?['issueDate'] ?? ''}'),
+                          Text('Expiry Date: ${cardDetails?['expiryDate'] ?? ''}'),
+                          Text('CVV: ${cardDetails?['cvv'] ?? ''}'),
+                        ],
+                      );
+                    }
+                  },
+                )
+
+            ),
+          ],
         ),
+      ),
     );
   }
+
+  void _toggleCardDetails() {
+    setState(() {
+      _showCardDetails = !_showCardDetails;
+    });
+  }
+
+  Future<Map<String, dynamic>?> _getCardDetails() async {
+    if (_showCardDetails) {
+      try {
+        await _methodChannel.invokeMethod('showCardDetails');
+      } on PlatformException catch (e) {
+        print('Error: ${e.message}');
+        return null;
+      }
+    } else {
+      try {
+        await _methodChannel.invokeMethod('maskCardDetails');
+      } on PlatformException catch (e) {
+        print('Error: ${e.message}');
+        return null;
+      }
+    }
+
+    // Return dummy data for testing
+    return {
+      'cardNumber': '**** **** **** 1234',
+      'issueDate': '01/22',
+      'expiryDate': '12/25',
+      'cvv': '***',
+    };
+  }
 }
-
-
-
-
